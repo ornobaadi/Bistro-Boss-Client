039585e29de1fdc7d903509396e0de92b22b3dd0
ornobaadi <aadi4789@gmail.com>
2024-12-29 22:27:35 +0600

    Navbar Footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {
+  RouterProvider,
+} from "react-router-dom";
+import { router } from './routes/Router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div className='max-w-screen-xl mx-auto'>
+    <RouterProvider router={router} />
+    </div>
   </StrictMode>,
 )
