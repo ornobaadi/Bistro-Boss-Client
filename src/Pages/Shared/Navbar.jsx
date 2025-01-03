26bb60d0d851f216b6cc52551f2694b224ce999a
ornobaadi <aadi4789@gmail.com>
2025-01-03 21:01:24 +0600

    Banner Carousel

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
     return (
         <>
-            <div className="navbar bg-base-100">
+            <div className="navbar max-w-7xl mx-auto fixed z-10 bg-opacity-30 bg-black text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
